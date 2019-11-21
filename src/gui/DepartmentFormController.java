82673f7ad11e58c333fab8caef6c216a152d6d42
Victtor Hugo <hgloureirocosta@gmail.com>
2019-11-21 15:23:20 -0300

    added method to save or update new form department in database

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 
 	private Department department;
+	private DepartmentService service;
 	
 	@FXML
 	private TextField textFieldId;
@@ -31,10 +38,30 @@ public class DepartmentFormController implements Initializable{
 	private Button btnCancel;
 	
 	@FXML
-	public void onBtnSaveAction() {
+	public void onBtnSaveAction(ActionEvent event) { // save or update database
+		if (department == null) {
+			throw new IllegalStateException("Department entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			department = getFormData();
+			service.saveOrUpdate(department);
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving department!", null, e.getMessage(), AlertType.ERROR);
+		}
 		
 	}
 	
+	private Department getFormData() { // instantiating department object with form data
+		Department department = new Department();
+		department.setId(Utils.tryParseToInt(textFieldId.getText()));
+		department.setName(textFieldName.getText());
+		return department;
+	}
+
 	@FXML
 	public void onBtnCancelAction() {
 		
@@ -42,6 +69,10 @@ public class DepartmentFormController implements Initializable{
 	
 	public void setDepartment(Department department) {
 		this.department = department;
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
